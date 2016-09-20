76759279c8da5f592ad15c7a569eeddc31987c12
yongju <yong8521@gmail.com>
2016-09-21 00:06:11 +0900

    commit sample2

---
+++ src/yong/java8/sample/Sample2.java
@@ -0,0 +1,33 @@
+package yong.java8.sample;
+
+import java.util.function.Function;
+
+/**
+ * Created by yongju on 16. 9. 20.
+ */
+public class Sample2 {
+    public static void main(String[] args) {
+        /*
+         * Functions, The Transformer
+         */
+        // using anonymous class
+        final Function<String, Integer> toInt = new Function<String, Integer>() {
+            @Override
+            public Integer apply(final String value) {
+                return Integer.valueOf(value);
+            }
+        };
+        System.out.println("1. toInt: "+toInt.apply("101"));
+
+        // using lambda expression
+        final Function<String, Integer> toInt2 = value -> Integer.valueOf(value);
+        System.out.println("2. toInt: "+toInt2.apply("102"));
+
+        final Function<Integer, Integer> identity1 = Function.identity();
+        System.out.println("3. identity: "+identity1.apply(103));
+
+        final Function<Integer, Integer> identity2 = t -> t;
+        System.out.println("4. identity: "+identity2.apply(104));
+
+    }
+}
